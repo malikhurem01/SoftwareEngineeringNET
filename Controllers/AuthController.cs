dfe8a8f1f992249380963d023a9a5bc0bb991085
Malik Huremovic <malikhu@maestralsolutions.com>
2023-05-04 21:38:07 +0200

    Added controller for user authentication

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,10 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ResumeMaker.API.DTOs;
+using ResumeMaker.Models;
+using ResumeMaker.Services;
 
 namespace ResumeMaker.API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class AuthController
+    public class AuthController : ControllerBase
     {
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<ServiceResponse<GetUserDto>>> RegisterUser(RegisterUserDto user)
+        {
+            var response = await _authService.RegisterUser(user);
+            return Ok(response);
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<ServiceResponse<GetUserDto>>> LoginUser(UserLoginDto user)
+        {
+            var response = await _authService.Login(user);
+            return Ok(response);
+        }
     }
 }
