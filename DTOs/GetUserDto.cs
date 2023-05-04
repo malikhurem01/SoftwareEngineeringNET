50f6a463227f561e2410ab9488a5613f8942da09
Malik Huremovic <malikhu@maestralsolutions.com>
2023-05-04 20:29:02 +0200

    Created getUserDto and userLoginDto

---
+++ DTOs/GetUserDto.cs
@@ -0,0 +1,12 @@
+﻿namespace ResumeMaker.API.DTOs
+{
+    public class GetUserDto
+    {
+        public string Id { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
