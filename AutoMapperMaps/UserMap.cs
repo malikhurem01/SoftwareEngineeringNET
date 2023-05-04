5c66fe0b8b5191ab1e76b188bee96daf91653544
Malik Huremovic <malikhu@maestralsolutions.com>
2023-05-04 20:28:34 +0200

    Added maps to map user to user data transfer objects

---
+++ AutoMapperMaps/UserMap.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using ResumeMaker.API.DTOs;
+using ResumeMaker.Models;
+
+namespace ResumeMaker.API.AutoMapperMaps
+{
+    public class UserMap : Profile
+    {
+        public UserMap() {
+            CreateMap<User, GetUserDto>();
+        }
+    }
+}
