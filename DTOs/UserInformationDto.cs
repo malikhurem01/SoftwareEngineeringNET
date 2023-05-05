c84664dad7a47c9180e3f8de9237b5a23698f274
Malik Huremovic <malikhu@maestralsolutions.com>
2023-05-05 22:38:41 +0200

    Added user information data transfer object

---
+++ DTOs/UserInformationDto.cs
@@ -0,0 +1,13 @@
+﻿using ResumeMaker.Models;
+
+namespace ResumeMaker.API.DTOs
+{
+    public class UserInformationDto
+    {
+        public List<Education> Education { get; set; } = new List<Education>();
+        public List<Experience> Experiences { get; set; } = new List<Experience>();
+        public List<Skill> Skills { get; set; } = new List<Skill>();
+        public List<Card> Cards { get; set; } = new List<Card>();
+        public List<Language> Languages { get; set; } = new List<Language>();
+    }
+}
