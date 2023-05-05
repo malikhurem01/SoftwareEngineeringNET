e73be4d4ba5441f39483f738c97848c4beccc94e
Malik Huremovic <malikhu@maestralsolutions.com>
2023-05-05 22:39:05 +0200

    Added initial controller features

---
+++ Controllers/InformationController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ResumeMaker.API.DTOs;
+using ResumeMaker.Models;
+
+namespace ResumeMaker.API.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class InformationController : ControllerBase
+    {
+        public InformationController() { }
+
+        [HttpPost]
+        public Task<ActionResult<ServiceResponse<string>>> AddUserInformation(UserInformationDto userInfo)
+        {
+            return null;
+        }
+
+        [HttpGet]
+        public Task<ActionResult<ServiceResponse<UserInformationDto>>> GetUserInformation()
+        {
+            return null;
+        }
+
+        [HttpDelete]
+        public Task<ActionResult<ServiceResponse<string>>> RemoveUserInformation(UserInformationDto userInfo)
+        {
+            return null;
+        }
+
+
+    }
+}
