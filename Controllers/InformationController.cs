a1e6aafa3af2cca21332fe060934c0a7ac07cbfc
Malik Huremovic <malikhu@maestralsolutions.com>
2023-05-06 04:49:39 +0200

    Added controller for making post requests for adding entities of basic user information

--- Controllers/InformationController.cs
+++ Controllers/InformationController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ResumeMaker.API.DTOs;
+using ResumeMaker.API.Services;
 using ResumeMaker.Models;
 
 namespace ResumeMaker.API.Controllers
@@ -8,26 +9,77 @@ namespace ResumeMaker.API.Controllers
     [Route("/api/[controller]")]
     public class InformationController : ControllerBase
     {
-        public InformationController() { }
+        private readonly IInformationService _informationService;
+        public InformationController(IInformationService informationService) {
+            _informationService = informationService;
+        }
 
-        [HttpPost]
-        public Task<ActionResult<ServiceResponse<string>>> AddUserInformation(UserInformationDto userInfo)
+        [HttpPost("education")]
+        public async Task<ActionResult<ServiceResponse<GetEducationDto>>> AddUserEducation(AddEducationDto userInfo)
         {
-            return null;
+            Request.Headers.TryGetValue("Authorization", out var token);
+            string tokenValue = token
+                .ToString()
+                .Split(" ")
+                .ElementAt(1);
+            return await _informationService.AddUserEducation(tokenValue, userInfo);
+        }
+
+        [HttpPost("experience")]
+        public async Task<ActionResult<ServiceResponse<GetExperienceDto>>> AddUserExperience(AddExperienceDto userInfo)
+        {
+            Request.Headers.TryGetValue("Authorization", out var token);
+            string tokenValue = token
+                .ToString()
+                .Split(" ")
+                .ElementAt(1);
+            return await _informationService.AddUserExperience(tokenValue, userInfo);
+        }
+
+        [HttpPost("skill")]
+        public async Task<ActionResult<ServiceResponse<GetSkillDto>>> AddUserSkill(AddSkillDto userInfo)
+        {
+            Request.Headers.TryGetValue("Authorization", out var token);
+            string tokenValue = token
+                .ToString()
+                .Split(" ")
+                .ElementAt(1);
+            return await _informationService.AddUserSkill(tokenValue, userInfo);
+        }
+
+        [HttpPost("language")]
+        public async Task<ActionResult<ServiceResponse<GetLanguageDto>>> AddUserLanguage(AddLanguageDto userInfo)
+        {
+            Request.Headers.TryGetValue("Authorization", out var token);
+            string tokenValue = token
+                .ToString()
+                .Split(" ")
+                .ElementAt(1);
+            return await _informationService.AddUserLanguage(tokenValue, userInfo);
+        }
+
+        [HttpPost("card")]
+        public async Task<ActionResult<ServiceResponse<GetCardDto>>> AddUserCard(AddCardDto userInfo)
+        {
+            Request.Headers.TryGetValue("Authorization", out var token);
+            string tokenValue = token
+                .ToString()
+                .Split(" ")
+                .ElementAt(1);
+            return await _informationService.AddUserCard(tokenValue, userInfo);
         }
 
         [HttpGet]
-        public Task<ActionResult<ServiceResponse<UserInformationDto>>> GetUserInformation()
+        public Task<ActionResult<ServiceResponse<AddInformationDto>>> GetUserInformation()
         {
             return null;
         }
 
         [HttpDelete]
-        public Task<ActionResult<ServiceResponse<string>>> RemoveUserInformation(UserInformationDto userInfo)
+        public Task<ActionResult<ServiceResponse<string>>> RemoveUserInformation(AddInformationDto userInfo)
         {
             return null;
         }
-
 
     }
 }
