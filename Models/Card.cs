9c822843a7d1754508d7df63253b0a22adcb95f6
Malik Huremovic <malikhu@maestralsolutions.com>
2023-05-07 03:39:14 +0200

    Removed unnecessary DateStart property from CARD entity

--- Models/Card.cs
+++ Models/Card.cs
@@ -7,7 +7,6 @@
         public User User { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Number { get; set; } = string.Empty;
-        public DateTime DateStart { get; set; }
         public DateTime DateEnd { get; set; }
         public int CVC { get; set; }
     }
